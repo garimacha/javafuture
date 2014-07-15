45ed07351477072981638fb3b4369c2948779119
garimacha <dgarima10@gmail.com>
2014-07-15 16:39:41 -0700

    Added another method

--- src/main/java/garimacha/javafuture/Hello.java
+++ src/main/java/garimacha/javafuture/Hello.java
@@ -3,9 +3,18 @@ package garimacha.javafuture;
 /**
  * Obligatory first class.
  */
-public class Hello {
+public  class Hello {
 
     public String greet() {
         return "Hello, World!";
+    }
+
+    public static  class GreetingReturn
+    {
+        public static String greetReturn()
+        {
+            return "I am good";
+        }
+
     }
 }
