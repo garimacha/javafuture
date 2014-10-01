22e78779ca6a61a5be8d36501e9c8e490fc80978
dgarima10@gmail.com <spark@1010>
2014-09-30 18:39:30 -0700

    New File Added

---
+++ src/test/java/garimacha/javafuture/ProteinTest.java
@@ -0,0 +1,16 @@
+package garimacha.javafuture;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by garich01 on 9/30/2014.
+ */
+public class ProteinTest {
+
+    @Test
+    public void testProtein() {
+    }
+    }
+
