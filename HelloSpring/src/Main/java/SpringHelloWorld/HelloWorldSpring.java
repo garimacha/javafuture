902e3ad2f8fffea56d0fc4498241760729b91ccf
dgarima10@gmail.com <spark@1010>
2014-10-09 16:13:32 -0700

    Added New Spring Classes

---
+++ HelloSpring/src/Main/java/SpringHelloWorld/HelloWorldSpring.java
@@ -0,0 +1,7 @@
+package Main.java.SpringHelloWorld;
+
+/**
+ * Created by garich01 on 10/9/2014.
+ */
+public class HelloWorldSpring {
+}
---
+++ HelloSpring/src/Test/java/SpringHelloWorld/TestHelloWorldSpring.java
@@ -0,0 +1,7 @@
+package Test.java.SpringHelloWorld;
+
+/**
+ * Created by garich01 on 10/9/2014.
+ */
+public class TestHelloWorldSpring {
+}
