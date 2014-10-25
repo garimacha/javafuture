d44c466cc6aba5b81131c5afe27f485d2c9ed6b2
dgarima10@gmail.com <spark@1010>
2014-10-24 18:32:59 -0700

    Added Bean.xml

--- HelloSpring/src/Main/java/SpringHelloWorld/Greeter.java
+++ HelloSpring/src/Main/java/SpringHelloWorld/Greeter.java
@@ -4,8 +4,20 @@ package Main.java.SpringHelloWorld;
  * Created by garich01 on 10/24/2014.
  */
 public class Greeter {
-    public String sayHello()
-    {
-        return "Hello Garima";
+
+    private String message;
+
+
+        public void setMessage (String message)
+        {
+            this.message = message;
+        }
+
+
+    public void getMessage() {
+        System.out.println("message: " + message);
     }
+
+
+
 }
--- HelloSpring/src/Main/java/SpringHelloWorld/HelloWorldSpring.java
+++ HelloSpring/src/Main/java/SpringHelloWorld/HelloWorldSpring.java
@@ -1,16 +1,32 @@
 package Main.java.SpringHelloWorld;
 
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by garich01 on 10/9/2014.
+ * There are following two important points to note about the main program:
+
+ First step is to create application context where we used framework API ClassPathXmlApplicationContext().
+ This API loads beans configuration file and eventually based on the provided API, it takes care of creating and initializing all
+ the objects ie. beans mentioned in the configuration file.
+
+ Second step is used to get required bean using getBean() method of the created context. This method uses bean ID to return a
+ generic object which finally can be casted to actual object. Once you have object, you can use this object to call
+ any class method.
  */
 public class HelloWorldSpring {
 
     public void main()
     {
-        Greeter gr= new Greeter();
-        System.out.println(gr.sayHello());
+        ApplicationContext context= new ClassPathXmlApplicationContext("Beans.xml");
+
+        Greeter obj=(Greeter)context.getBean("helloWorld");
+        obj.getMessage();
+
+
+       /* Greeter gr= new Greeter();
+        System.out.println(gr.sayHello());*/
 
     }
 }
