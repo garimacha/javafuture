3b3bb80401d4aa5a7b5c730012152514ca2ec023
dgarima10@gmail.com <spark@1010>
2014-10-24 17:55:26 -0700

    Added hello spring

---
+++ HelloSpring/src/Main/java/SpringHelloWorld/Greeter.java
@@ -0,0 +1,11 @@
+package Main.java.SpringHelloWorld;
+
+/**
+ * Created by garich01 on 10/24/2014.
+ */
+public class Greeter {
+    public String sayHello()
+    {
+        return "Hello Garima";
+    }
+}
--- HelloSpring/src/Main/java/SpringHelloWorld/HelloWorldSpring.java
+++ HelloSpring/src/Main/java/SpringHelloWorld/HelloWorldSpring.java
@@ -1,7 +1,16 @@
 package Main.java.SpringHelloWorld;
 
+
+
 /**
  * Created by garich01 on 10/9/2014.
  */
 public class HelloWorldSpring {
+
+    public void main()
+    {
+        Greeter gr= new Greeter();
+        System.out.println(gr.sayHello());
+
+    }
 }
